822ef23bf5d919144be2089838dacda12f72c939
six-shot <okhuomonajayi54@gmail.com>
2025-02-20 00:58:15 +0100

    moving effect

--- src/app/section/movinglight/index.jsx
+++ src/app/section/movinglight/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import styles from "./MovingLight.module.css";
 
 export default function MovingLight() {
   return (
     <div className="flex justify-center iteqms-center">
       <div className="relative w-[1240px] h-[817px] rounded-[40px] ">
-        <div className="button-frames absolute -inset-4 overflow-hidden button-frames-primary rounded-[40px]"></div>
-        <div className="button-frames absolute -inset-4 overflow-hidden button-frames-primary rounded-[40px]"></div>
+        <div className="frames absolute -inset-4 overflow-hidden frames-primary rounded-[40px]"></div>
+        
       </div>
     </div>
   );
