528075ffe1f4869ad2379ecb1fa0228f8bf0995f
six-shot <okhuomonajayi54@gmail.com>
2025-02-20 03:41:41 +0100

    made fixes to landing page

--- src/app/section/privacy/index.jsx
+++ src/app/section/privacy/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
-import privacy from "../../../../public/privacy.png";
+import privacy from "../../../../public/privacy.gif";
 
 export default function Privacy() {
   const ref = useRef(null);
@@ -20,9 +20,9 @@ export default function Privacy() {
   const imageY = useTransform(scrollYProgress, [0.25, 0.55], [50, 0]);
 
   return (
-    <div className="h-[120vh] flex justify-center items-center">
-      <motion.div ref={ref} className="flex text-white">
-        <div className="flex items-center">
+    <div className="h-[140vh] py-[140px] flex justify-center items-center">
+      <motion.div ref={ref} className="flex flex-col text-white">
+        <div className="flex items-center mb-[140px]">
           <motion.div
             style={{ opacity: imageOpacity, y: imageY }}
             className="px-5"
@@ -46,6 +46,31 @@ export default function Privacy() {
               request KYC
             </p>
           </div>
+        </div>
+        <div className="flex items-center">
+          <div className="px-5">
+            <motion.h2
+              className="text-[96px] font-sfpro_display_bold   prr leading-[107px] w-[437px]"
+              style={{ opacity, y }}
+            >
+              And <br /> Security <br /> Matters
+              <br /> The most
+            </motion.h2>
+            <p className="mt-5 mb-2.5 text-[26px] font-sfpro_display_regular leading-[112%] tracking-[1.1px] text-[#cecece] w-[437px]">
+              Your private keys and seed phrase are stored in app's local
+              storage and encrypted with your beautiful face
+            </p>
+          </div>
+          <motion.div
+            style={{ opacity: imageOpacity, y: imageY }}
+            className="px-5 w-[490px]  h-[462px]"
+          >
+            <Image
+              className=""
+              src={privacy}
+              alt="privacy"
+            />
+          </motion.div>
         </div>
       </motion.div>
     </div>
