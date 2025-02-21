661e44f83964fd9bfcea9da7368649895e0aa481
six-shot <okhuomonajayi54@gmail.com>
2025-02-21 17:19:43 +0100

    fix:loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -28,14 +28,14 @@ const Preloader = () => {
   if (!loading) return null;
 
   return (
-    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black">
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black transition-opacity duration-500 ease-in-out">
       <div className="flex flex-col items-center">
         <div className="relative h-20 w-20">
           <div className="absolute animate-ripple-1 h-20 w-20 rounded-full bg-gray-500/40 opacity-0"></div>
           <div className="absolute animate-ripple-2 h-20 w-20 rounded-full bg-gray-500/40 opacity-0"></div>
           <div className="absolute animate-ripple-3 h-20 w-20 rounded-full bg-gray-500/40 opacity-0"></div>
         </div>
-        <div className="text-[#646566] mt-6 animate-soft-blink">
+        <div className="text-[#646566] mt-6 animate-soft-blink font-sfpro_display_medium">
           Connecting to Web3...
         </div>
       </div>
